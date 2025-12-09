4fd599f737022388e9506ba86bc4dd6a852b4d9c
Phantom <murtezatawana8@gmail.com>
2025-12-09 08:35:28 -0800

    Update nparray.py

--- NumPy/nparray.py
+++ NumPy/nparray.py
@@ -1,1 +1,12 @@
 # Use np.array() when you want to convert Python lists into Numpy arrays
+
+import numpy as np
+
+a1 = np.array([1, 2, 3])
+a2 = np.array([[1, 2], [3, 4]])
+a3 = np.array([[[1, 2], [3, 4]],
+               [[5, 6], [7, 8]]])
+
+print(a1)
+print(a2)
+print(a3)
