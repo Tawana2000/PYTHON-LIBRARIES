30861f110b350f8747ef78005ee705db156cbb86
Phantom <murtezatawana8@gmail.com>
2025-12-12 11:21:52 -0800

    Add slicing examples to numpy-functions.py
    Added examples of slicing and indexing in NumPy.

--- NumPy/numpy-functions.py
+++ NumPy/numpy-functions.py
@@ -1,1 +1,9 @@
 # Slicing in NumPy follows the same indexing rules as Python lists, but extends them to multiple dimensions, allowing you to select rows, columns , or sub-arrays efficiently.
+
+import numpy as np
+
+a = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+print(a[1:4])
+print(a[:, 1])
